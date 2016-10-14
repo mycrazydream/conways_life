a86c962583d59e764f50d82954502a3b9eb27b38
Brent Ransom <mycrazydream@micomputadora.(none)>
2016-10-13 21:34:48 -0700

    Creative Commons License added

--- cgol.js
+++ cgol.js
@@ -1,12 +1,18 @@
 /*
-conways_game_of_life.js
+Conway's Game of Life (w D3.js)
+http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+cgol.js
 
-Created by Brent Ransom on 2016-10-10.
-Copyright &copy; 2016 mycrazydream. All rights reserved.
+Creative Commons License Markup
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br />
+<span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">Conway's Game of Life implemented with D3.js</span> by 
+<a xmlns:cc="http://creativecommons.org/ns#" href="http://mycrazydream.net/conway/" property="cc:attributionName" rel="cc:attributionURL">Brent Ransom</a> 
+is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.<br />
+Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="http://www.conwaylife.com/" rel="dct:source">http://www.conwaylife.com/</a><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">Conway's Game of Life implemented with D3.js</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://mycrazydream.net/conway/" property="cc:attributionName" rel="cc:attributionURL">Brent Ransom</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="http://www.conwaylife.com/" rel="dct:source">http://www.conwaylife.com/</a>.<br />
+Permissions beyond the scope of this license may be available at <a xmlns:cc="http://creativecommons.org/ns#" href="https://d3js.org/" rel="cc:morePermissions">https://d3js.org/</a>.
 
 Conway's Game of Life
-http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-
 Axiom 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
 Axiom 2. Any live cell with two or three live neighbours lives on to the next generation.
 Axiom 3. Any live cell with more than three live neighbours dies, as if by overcrowding.
